6c194aebd1203a68a1f4d7356b39e1d2b2e67d6f
infinicky <infinicky.codes@gmail.com>
2023-07-20 12:40:47 +0200

    Feat: Add Create New Page and Search Results pages
    Search Results bug: Treats string as List
    Create New Page not yet functional

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django import forms
 from . import util
 
 
@@ -7,6 +7,7 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
 
 def wiki(request, title):
     # Get correct entry file
@@ -21,3 +22,49 @@ def wiki(request, title):
         "entry": html,
         "title": title
     })
+
+
+def search(request):
+    """
+    If the query matches the name of an encyclopedia entry, the user will 
+    be redirected to that entry's page.
+    If not, the user is taken to a search results page that displays 
+    a list of all encyclopedia entries that have the query as a substring.
+    """
+    search_results = []
+    query = request.GET.get("q").lower()
+    # If existing entry, go to that page
+    for entry in util.list_entries():
+        entry_lower = entry.lower()
+        if query == entry_lower:
+            return wiki(request, query)
+        # If text matches entry, add to search results
+        elif query in entry_lower:
+            search_results += entry
+    
+    # Return list of search results
+    return render(request, "encyclopedia/search_results.html", {
+        "results": search_results
+    }) 
+
+
+def create(request):
+    return render(request, "encyclopedia/create_wiki.html", {
+        "form": CreateWikiForm()
+    })
+
+    if request.method == "post":
+        form = CreateWikiForm(request.POST)
+        # check validity? need title and content
+        title = form.cleaned_data["Title"]
+        content = form.cleaned_data["Content"]
+        # add wiki to entries
+        util.save_entry(title, content)
+
+    else:
+        return render(request, "encyclopedia/create_wiki.html")
+
+
+class CreateWikiForm(forms.Form):
+    title = forms.CharField(label="Title")
+    content = forms.CharField(label="Content")
