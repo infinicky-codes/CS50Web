5f2baf12426f9364a1f4cc7b336581e954d9525b
infinicky <infinicky.codes@gmail.com>
2023-08-02 13:47:02 +0200

    Feat: Add ForeignKeys to models and migrate to db

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -8,8 +8,8 @@ class User(AbstractUser):
 
 class Listing(models.Model):
     # Each listing is created by one specific User
-    user = models.ForeignKey(User, on_delete=models.CASCADE),
-    item_name = models.CharField(max_length=64),
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item_name = models.CharField(max_length=64)
     asking_price = models.DecimalField(max_digits=7, decimal_places=2)
 
     def __str__(self):
@@ -18,17 +18,19 @@ class Listing(models.Model):
 
 class Bid(models.Model):
     # Each bid is on one specific Listing
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE),
-    # Made by one specific User
-    user = models.ForeignKey(User, on_delete=models.CASCADE),
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    # made by one specific User
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # and is for a certain amount
     amount = models.DecimalField(max_digits=7, decimal_places=2)
 
 
 class Comment(models.Model):
     # Each comment is on one specific Listing
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE),
-    # Made by one specific User
-    user = models.ForeignKey(User, on_delete=models.CASCADE),
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    # made by one specific User
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # and has a content of maximum 256 characters
     content = models.CharField(max_length=256)
     
 
