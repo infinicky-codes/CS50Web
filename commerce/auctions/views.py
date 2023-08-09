017f85e2e82ab731f65d390fd09c4974d6c019bb
infinicky <infinicky.codes@gmail.com>
2023-08-09 12:52:30 +0200

    Feat: List active listings on index.html
    Bug: Bids not correctly linked to listings. Need to get foreign key/ add
    related name to model? Same probably goes for comments. #9

--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -4,12 +4,20 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Listing
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = Listing.objects.all()
+    for listing in listings:
+        current = listing.current_price
+    return render(request, "auctions/index.html", {
+        "listings": Listing.objects.all()
+})
 
+
+
+### User specific Views ###
 
 def login_view(request):
     if request.method == "POST":
