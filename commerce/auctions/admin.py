85b4d250d61ccfb8832a8616411e377cc3aad5c0
infinicky <infinicky.codes@gmail.com>
2023-08-09 12:05:41 +0200

    Feat: Populate db and set optional fields

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Listing, Category, Comment, Bid, User
+
 # Register your models here.
+admin.site.register(Listing)
+admin.site.register(Category)
+admin.site.register(Comment)
+admin.site.register(Bid)
+admin.site.register(User)
--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -9,32 +9,44 @@ class User(AbstractUser):
 class Category(models.Model):
     title = models.CharField(max_length=64)
 
+    def __str__(self):
+        return f"Category {self.id}: {self.title}"
 
+
+# TODO: get last (= highest) bid to populate current_price,
+#       or current_price = asking_price?
+#       Maybe name change is in order: highest_bid
 class Listing(models.Model):
     # Each listing is created by one specific User
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # and has a title, description and current price
     title = models.CharField(max_length=64)
     description = models.CharField(max_length=255, default='')
-    # TODO: get last (= highest) bid
-    current_price = models.DecimalField(max_digits=7, decimal_places=2)
+    asking_price = models.DecimalField(max_digits=7, decimal_places=2)
     # optional fields
-    image_url = models.CharField(max_length=255, default='')
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, default=None)
+    current_price = models.DecimalField(max_digits=7, decimal_places=2, 
+                                        null=True, blank=True)
+    image_url = models.CharField(max_length=255, null=True, blank=True)
+    # A listing can belong to only 1 category
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, 
+                                 null=True, blank=True)
 
     def __str__(self):
-        return f"{self.id}: {self.title}, €{self.current_price}"
+        if self.current_price == None:
+            self.current_price = self.asking_price
+        return f"Listing #{self.id}: {self.title}, €{self.current_price}"
 
 
+# TODO: user can't bid on own listing
+# TODO: new bid must be higher than listing.current_price
 class Bid(models.Model):
     # Each bid is on one specific Listing
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
     # made by one specific User
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    # and is for a certain amount
-    # TODO: new bid must be higher than listing.current_price
     amount = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return f"Bid #{self.id}: €{self.amount} on {self.listing.title}"
 
 
 class Comment(models.Model):
@@ -44,3 +56,5 @@ class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"Comment (ID #{self.id}) on {self.listing.title} by User: {self.user}"
